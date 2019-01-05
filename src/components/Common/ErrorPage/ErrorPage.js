a9ece7c1706320ae821e1d38b4ed36db4d542f5c
Eli <iamelimars@yahoo.com>
2019-01-04 22:43:34 -0500

    Made hero svg bigger

--- src/components/Common/ErrorPage/ErrorPage.js
+++ src/components/Common/ErrorPage/ErrorPage.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './ErrorPage.css'
 
 const errorPage = () => (
     <div className={styles.page}>
         <h1>Error Loading Content</h1>
-        <h2>Check yout internet connection............</h2>
+        <h2>Check your internet connection............</h2>
         <a href="/">Back To Home</a>
     </div>
 )
