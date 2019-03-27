f9c7be71afe722f5e8f477d7990dfaf98eed2349
Elisha Marshall <iamelimars@yahoo.com>
2019-03-26 22:43:26 -0400

    Added tests to error page

---
+++ src/components/Common/ErrorPage/ErrorPage.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ErrorPage from './ErrorPage';
+import { findByTestAttr } from '../../../../Utils';
+
+const setUp = (props={}) => {
+    const component = shallow(<ErrorPage {...props} />);
+    return component
+}
+
+describe("<ErrorPage />",  () => {
+    let component;
+    beforeEach(() => {
+        component = setUp();
+    })
+
+    it("Should render page without errors", () => {
+        const wrapper = findByTestAttr(component, 'errorContainer');
+        expect(wrapper.length).toBe(1);
+    })
+})
